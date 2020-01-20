8ccf197ee3f196aacd7666ac29ad3970d2188a90
moksleivis <sdgf@dvf.lt>
2020-01-20 15:18:28 +0200

    actor

--- database/database.py
+++ database/database.py
@@ -1,32 +1,29 @@
 import sqlite3
 
+db_name = "movies"
 
-class database:
 
-    def __init__(self, db_name):
-        self.db_name = db_name
+def execute_query(query, params=None, select=None):
+    try:
+        connection = sqlite3.connect(db_name)
+        cursor = connection.cursor()
 
-    def execute_query(self, query, params, select=None):
-        try:
-            connection = sqlite3.connect(self.db_name)
-            cursor = connection.cursor()
+        if select:
+            record_id = cursor.execute(query, params).fetchone()[0]
+            return record_id
 
-            if select:
-                record_id = cursor.execute(query, params).fetchone()[0]
-                return record_id
-
+        if params:
             cursor.execute(query, params)
-            connection.commit()
-        except sqlite3.DatabaseError as error:
-            print(error)
-        finally:
-            # noinspection PyUnboundLocalVariable
-            cursor.close()
-            connection.close()
+        else:
+            cursor.execute(query)
+
+        connection.commit()
+    except sqlite3.DatabaseError as error:
+        print(error)
+    finally:
+        # noinspection PyUnboundLocalVariable
+        cursor.close()
+        connection.close()
 
 
     # def create_table():
-
-
-db_name = "movies"
-movies_db = database(db_name)
--- models/actor.py
+++ models/actor.py
@@ -7,6 +7,14 @@ class actor:
         self.name = name
         self.lastname = lastname
 
+    def create_table(self):
+        query = """CREATE TABLE IF NOT EXISTS actors (
+                                                    actor_id integer PRIMARY KEY,
+                                                    first_name text NOT NULL,
+                                                    last_name text NOT NULL
+                                                    )"""
+
+
     def create_actor(self, actor_id=None):
         query = "INSERT INTO hearts VALUES(?, ?, ?)"
         params = (actor_id, self.name, self.lastname)
